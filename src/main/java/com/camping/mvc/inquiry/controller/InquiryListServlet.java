9afb75dd4690ce25abde8262041fef6bcca7b9c5
eunbin kang <eunbin4603@naver.com>
2022-07-30 14:16:01 +0900

    2022-07-26 14:14 강은빈
      1. inquiryBoard.jsp
         inquiryBoardView.jsp
    - 두 파일 헤더 푸터 분리.
    
      2.inquiryListServlet.jave
     - 코드 작성 중 (임시저장)

--- src/main/java/com/camping/mvc/inquiry/controller/InquiryListServlet.java
+++ src/main/java/com/camping/mvc/inquiry/controller/InquiryListServlet.java
@@ -1,9 +1,44 @@
 package com.camping.mvc.inquiry.controller;
 
+import java.io.IOException;
+import java.util.List;
 
-public class InquiryListServlet{
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-		
+import com.camping.common.util.PageInfo;
+import com.camping.mvc.inquiry.model.service.InquiryService;
+import com.camping.mvc.inquiry.model.vo.Inquiry;
 
+@WebServlet("/board/listServlet")
+public class InquiryListServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
 	
+	private InquiryService service = new InquiryService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		int page = 1;
+		int InquiryCount = 0;
+		PageInfo pageInfo = null;
+		List<Inquiry> list = null;
+		String searchValue = null;
+		
+		try {
+			searchValue = req.getParameter("searchValue");
+			
+			page = Integer.parseInt(req.getParameter("page")); 
+		}catch (Exception e) {
+		}
+		
+		
+		
+		
+		
+	}
 }
+	
