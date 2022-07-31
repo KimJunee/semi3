f2fecb0796fa22b5cc0c42dbbac8c3b5c61dbef2
eunbin kang <eunbin4603@naver.com>
2022-07-31 13:37:51 +0900

    문의게시판.java 수정 - 강은빈
    InquiryDeleteServlet.java
    InquiryRelpyDeleteServlet.java
    InquiryReplyWriteServlet.java
    InquiryViewServlet.java
    InquiryWriteServlet.java

--- src/main/java/com/camping/mvc/inquiry/controller/InquiryViewServlet.java
+++ src/main/java/com/camping/mvc/inquiry/controller/InquiryViewServlet.java
@@ -1,6 +1,44 @@
 package com.camping.mvc.inquiry.controller;
 
+import java.io.IOException;
 
-public class InquiryViewServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.camping.mvc.inquiry.model.service.InquiryService;
+import com.camping.mvc.inquiry.model.vo.Inquiry;
+
+@WebServlet("/board/viewInquiry")
+public class InquiryViewServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
 	
-}
+	private InquiryService service = new InquiryService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println(req.getParameter("inquiryNo"));
+		int inquiryNo = Integer.parseInt(req.getParameter("inquiryNo"));//파라메터 받아옴(데이터처리)
+
+		//게시글 실제로 가져오기
+		Inquiry inquiry = service.findInquiryByNo(inquiryNo, true);
+		//findBoardByNo로 조회해서 보면 조회수 자동으로 증가
+
+		//예외처리 해주기
+		if(inquiry == null) {
+			resp.sendRedirect(req.getContextPath() + "/");//메인으로보냄
+			return;
+		}
+		req.setAttribute("inquiry", inquiry);
+		req.getRequestDispatcher("/views/04_Community/inquiryBoardView.jsp").forward(req, resp);
+	}
+		
+	}
+	
+	
+
--- src/main/java/com/camping/mvc/inquiry/controller/InquiryWriteServlet.java
+++ src/main/java/com/camping/mvc/inquiry/controller/InquiryWriteServlet.java
@@ -1,7 +1,103 @@
 package com.camping.mvc.inquiry.controller;
 
-public class InquiryWriteServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.camping.common.util.MyHttpServlet;
+import com.camping.mvc.inquiry.model.service.InquiryService;
+import com.camping.mvc.inquiry.model.vo.Inquiry;
+import com.camping.mvc.member.model.vo.Member;
+
+@WebServlet("/board/inquiryWrite")
+public class InquiryWriteServlet extends MyHttpServlet {
+
+	private static final long serialVersionUID = 1L;
 	
+	private InquiryService service = new InquiryService();
+
+	@Override
+	public String getServletName() {
+		return "InquiryWrite";
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			Member loginMember = getSessionMember(req);//로그인 멤버 땡겨옴
+			if(loginMember != null) {
+				// 정상 흐름,글쓰기로 이동
+				req.getRequestDispatcher("/views/04_Community/inquiryBoard.jsp").forward(req, resp);
+				return;
+			}
+		} catch (Exception e) {}//로그인이 실패할 경우!
+		sendCommonPage("로그인 이후 사용할수 있습니다.", "/views/04_Main/main.jsp", req, resp);//게시판홈으로 넘기기
+		//sendCommonPage ( 띄울메시지,이동할링크,req,resp)
+	}
+		
+		
+
 
 	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+			Member loginMember = getSessionMember(req);//로그인멤버 가져옴
+			
+			if(loginMember ==null) {
+				sendCommonPage("잘못된 접근입니다. (code=101)", "/board/listServlet", req, resp);
+				return;
+			}
+			
+			Inquiry inquiry = new Inquiry();
+			inquiry.setInquir_title(req.getParameter("title").strip());
+			inquiry.setUser_no(loginMember.getUser_no());
+			inquiry.setInquir_content(req.getParameter("content").trim());
+			
+			System.out.println(inquiry.toString());
+			int result = service.saveInquiry(inquiry);
+			
+			
+			if(result > 0) {
+				sendCommonPage("게시글이 정상적으로 등록되었습니다.", "/board/listServlet", req, resp);
+				
+			}else {
+				sendCommonPage("게시글 등록에 실패하였습니다. (code=102)", "/board/listServlet", req, resp);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			sendCommonPage("정상적으로 처리할수 없습니다. (code=103)",  "/board/listServlet", req, resp);
+		}
+		
+		
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
