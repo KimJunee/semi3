8dedb08542e125116598b824262d378c319f647e
eunbin kang <eunbin4603@naver.com>
2022-08-01 11:29:54 +0900

    문의게시판 로그인시 이용가능하게 변경_강은빈
    inquiryListServlet.java
    InquiryViewServlet.java

--- src/main/java/com/camping/mvc/inquiry/controller/InquiryListServlet.java
+++ src/main/java/com/camping/mvc/inquiry/controller/InquiryListServlet.java
@@ -8,28 +8,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.camping.common.util.MyHttpServlet;
 import com.camping.common.util.PageInfo;
 import com.camping.mvc.inquiry.model.service.InquiryService;
 import com.camping.mvc.inquiry.model.vo.Inquiry;
 import com.camping.mvc.member.model.vo.Member;
 
 @WebServlet("/board/listServlet")
-public class InquiryListServlet extends HttpServlet{
+public class InquiryListServlet extends MyHttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	private InquiryService service = new InquiryService();
 	
 	@Override
+	public String getServletName() {
+		// TODO Auto-generated method stub
+		return "InquiryList";
+	}
+	
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		
 		int page = 1;
 		int InquiryCount = 0;
 		PageInfo pageInfo = null;
 		List<Inquiry> list = null;
 		String searchValue = null;
 		
+
 		try {
+			
+			HttpSession session = req.getSession(); //HttpSession이 존재하면 현재 HttpSession을 반환하고 존재하지 않으면 새로이 세션을 생성합니다
+			System.out.println(session.getAttribute("loginMember"));
+			Member member = (Member) session.getAttribute("loginMember");
+			
+			if(member == null) {
+				sendCommonPage("로그인후 이용해주세요.", "/views/01_Main/main.jsp", req, resp);
+				return;
+			}
 			searchValue = req.getParameter("searchValue");
 			
 			page = Integer.parseInt(req.getParameter("page")); 
@@ -44,10 +62,7 @@ public class InquiryListServlet extends HttpServlet{
 		req.setAttribute("list", list);//리스트로 넘기고
 		req.setAttribute("pageInfo", pageInfo);//페이지인포로넘기고
 		req.getRequestDispatcher("/views/04_Community/inquiryBoard.jsp").forward(req, resp);
-		
-		
-		
-		
+
 	}
+
 }
-	
--- src/main/java/com/camping/mvc/inquiry/controller/InquiryViewServlet.java
+++ src/main/java/com/camping/mvc/inquiry/controller/InquiryViewServlet.java
@@ -8,20 +8,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import com.camping.common.util.MyHttpServlet;
 import com.camping.mvc.inquiry.model.service.InquiryService;
 import com.camping.mvc.inquiry.model.vo.Inquiry;
+import com.camping.mvc.member.model.vo.Member;
 
 @WebServlet("/board/viewInquiry")
-public class InquiryViewServlet extends HttpServlet{
+public class InquiryViewServlet extends MyHttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
 	private InquiryService service = new InquiryService();
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public String getServletName() {
 
+		return "InquiryView";
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+
+		
+		
 		System.out.println(req.getParameter("inquiryNo"));
 		int inquiryNo = Integer.parseInt(req.getParameter("inquiryNo"));//파라메터 받아옴(데이터처리)
 
@@ -37,8 +48,22 @@ public class InquiryViewServlet extends HttpServlet{
 		req.setAttribute("inquiry", inquiry);
 		req.getRequestDispatcher("/views/04_Community/inquiryBoardView.jsp").forward(req, resp);
 	}
-		
+
+
 	}
+
 	
+	/*
+	 * 여기 페이지에 이걸 추가하면 되지 않을까 싶은데 잘 안되... 
+	 * try { 
+	 * Member loginMember = getSessionMember(req);//로그인 멤버 땡겨옴 
+	 * if(loginMember != null) { // 정상 흐름,글쓰기로 이동
+	 * req.getRequestDispatcher("/views/04_Community/inquiryBoard.jsp").forward(req,
+	 * resp); return; } } catch (Exception e) {}//로그인이 실패할 경우!
+	 * sendCommonPage("로그인 이후 사용할수 있습니다.", "/views/01_Main/main.jsp", req,
+	 * resp);//게시판홈으로 넘기기 //sendCommonPage ( 띄울메시지,이동할링크,req,resp)
+	 */
+
+
 	
 
